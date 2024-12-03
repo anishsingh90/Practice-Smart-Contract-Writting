93f140051169cae7e7a4695bc1370c6609534641
anishsingh90 <anishraaz90@gmail.com>
2024-12-03 17:27:16 +0530

    hg

--- Project 01/anish.sol
+++ Project 01/anish.sol
@@ -6,7 +6,7 @@ contract SolidityTest {
    }
    function getResult() public view returns(uint){
       uint a = 1;
-      uint b = 2;
+      uint b = 2; 
       uint result = a + b;
       return result;
    }
